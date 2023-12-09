54904f3cfcecc8d669232534bb3e9913e13e66b8
GP-Sword <138867119+GP-Sword@users.noreply.github.com>
2023-12-09 16:24:17 +0700

    Add files

---
+++ src/main/java/main_package/oop_gui/Controllers/ButtonController.java
@@ -0,0 +1,5 @@
+package main_package.oop_gui.Controllers;
+
+public abstract class ButtonController {
+    public abstract void function();
+}
---
+++ src/main/java/main_package/oop_gui/Controllers/ChatBubble.java
@@ -0,0 +1,13 @@
+package main_package.oop_gui.Controllers;
+
+public class ChatBubble {
+    private String side;
+
+    ChatBubble() {
+
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+}
